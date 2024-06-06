1ac4e5f907611b2e5ba4b56b1c5383acef8e8c94
unknown <wieedie@web.de>
2024-06-06 13:13:45 +0200

    bot

--- script2.js
+++ script2.js
@@ -6,10 +6,24 @@ let board = [
 ];
 let playerA = '';
 let playerB = '';
+let checkBox = document.getElementById('botCheckBox');
+
+function checkBot() {
+    if(document.getElementById('playerB').style.display === 'none'){
+        document.getElementById('playerB').style.display = 'inline'
+    } else {
+        document.getElementById('playerB').style.display = 'none';
+    }
+}
 
 function start() {
     playerA = document.getElementById('playerA').value;
-    playerB = document.getElementById('playerB').value;
+    if(!checkBox.checked){
+        playerB = document.getElementById('playerB').value;
+    }
+    else {
+        playerB = 'Bot';
+    }
     if (playerA && playerB) {
         document.getElementById('startScreen').style.display = 'none';
         document.getElementById('gameScreen').style.display = 'block';
@@ -45,18 +59,186 @@ function move(button, row, col) {
         if (checkWin()) {
             setTimeout(() => {
                 alert(`${currentPlayer === 'X' ? playerA : playerB} hat gewonnen!`);
-                resetGame();
+                newGame();
             }, 100);
         } else if (isDraw()) {
             setTimeout(() => {
                 alert('Unentschieden!');
-                resetGame();
+                newGame();
             }, 100);
         } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             updatePlayerInfo();
         }
+        if (currentPlayer !== 'X' && playerB === 'Bot') {
+            setTimeout(() => {
+                makeBotMove();
+            }, 101); // Add a slight delay for bot move
+        }
     }
+}
+
+function makeBotMove() {
+    //Angreifen
+
+    //Horizontale Gewinnmöglichkeiten
+    if(currentPlayer === 'X'){
+    }
+    else if(board[0][0]==='O' && board[0][1]==='O' && board[0][2]===''){
+        move(button3, 0, 2);
+    } else if(board[0][0] === 'O' && board[0][2] === 'O' && board[0][1]===''){
+        move(button2, 0, 1);
+    } else if(board[0][1] === 'O' && board[0][2] === 'O' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[1][0] === 'O' && board[1][1] === 'O' && board[1][2]===''){
+        move(button6, 1, 2);
+    } else if(board[1][0] === 'O' && board[1][2] === 'O' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'O' && board[1][2] === 'O' && board[1][0]===''){
+        move(button4, 1, 0);
+    } else if(board[2][0] === 'O' && board[2][1] === 'O' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[2][0] === 'O' && board[2][2] === 'O' && board[2][1]===''){
+        move(button8, 2, 1);
+    } else if(board[2][1] === 'O' && board[2][2] === 'O' && board[2][0]===''){
+        move(button7, 2, 0);
+    }
+    // Vertikale Gewinnmöglichkeiten
+    else if(board[0][0] === 'O' && board[1][0] === 'O' && board[2][0]===''){
+        move(button7, 2, 0);
+    } else if(board[0][0] === 'O' && board[2][0] === 'O' && board[1][0]===''){
+        move(button4, 1, 0);
+    } else if(board[1][0] === 'O' && board[2][0] === 'O' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[0][1] === 'O' && board[1][1] === 'O' && board[2][1]===''){
+        move(button8, 2, 1);
+    } else if(board[0][1] === 'O' && board[2][1] === 'O' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'O' && board[2][1] === 'O' && board[0][1]===''){
+        move(button2, 0, 1);
+    } else if(board[0][2] === 'O' && board[1][2] === 'O' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[0][2] === 'O' && board[2][2] === 'O' && board[1][2]===''){
+        move(button6, 1, 2);
+    } else if(board[1][2] === 'O' && board[2][2] === 'O' && board[0][2]===''){
+        move(button3, 0, 2);
+    }
+    // Diagonale Gewinnmöglichkeiten
+    else if(board[0][0] === 'O' && board[1][1] === 'O' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[0][0] === 'O' && board[2][2] === 'O' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'O' && board[2][2] === 'O' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[2][0] === 'O' && board[1][1] === 'O' && board[0][2]===''){
+        move(button3, 0, 2);
+    } else if(board[2][0] === 'O' && board[0][2] === 'O' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'O' && board[0][2] === 'O' && board[2][0]===''){
+        move(button7, 2, 0);
+    }
+
+        //Verteidigen
+
+    // Horizontale Gewinnmöglichkeiten
+    else if(board[0][0] === 'X' && board[0][1] === 'X' && board[0][2]===''){
+        move(button3, 0, 2);
+    } else if(board[0][0] === 'X' && board[0][2] === 'X' && board[0][1]===''){
+        move(button2, 0, 1);
+    } else if(board[0][1] === 'X' && board[0][2] === 'X' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[1][0] === 'X' && board[1][1] === 'X' && board[1][2]===''){
+        move(button6, 1, 2);
+    } else if(board[1][0] === 'X' && board[1][2] === 'X' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'X' && board[1][2] === 'X' && board[1][0]===''){
+        move(button4, 1, 0);
+    } else if(board[2][0] === 'X' && board[2][1] === 'X' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[2][0] === 'X' && board[2][2] === 'X' && board[2][1]===''){
+        move(button8, 2, 1);
+    } else if(board[2][1] === 'X' && board[2][2] === 'X' && board[2][0]===''){
+        move(button7, 2, 0);
+    }
+    // Vertikale Gewinnmöglichkeiten
+    else if(board[0][0] === 'X' && board[1][0] === 'X' && board[2][0]===''){
+        move(button7, 2, 0);
+    } else if(board[0][0] === 'X' && board[2][0] === 'X' && board[1][0]===''){
+        move(button4, 1, 0);
+    } else if(board[1][0] === 'X' && board[2][0] === 'X' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[0][1] === 'X' && board[1][1] === 'X' && board[2][1]===''){
+        move(button8, 2, 1);
+    } else if(board[0][1] === 'X' && board[2][1] === 'X' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'X' && board[2][1] === 'X' && board[0][1]===''){
+        move(button2, 0, 1);
+    } else if(board[0][2] === 'X' && board[1][2] === 'X' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[0][2] === 'X' && board[2][2] === 'X' && board[1][2]===''){
+        move(button6, 1, 2);
+    } else if(board[1][2] === 'X' && board[2][2] === 'X' && board[0][2]===''){
+        move(button3, 0, 2);
+    }
+    // Diagonale Gewinnmöglichkeiten
+    else if(board[0][0] === 'X' && board[1][1] === 'X' && board[2][2]===''){
+        move(button9, 2, 2);
+    } else if(board[0][0] === 'X' && board[2][2] === 'X' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'X' && board[2][2] === 'X' && board[0][0]===''){
+        move(button1, 0, 0);
+    } else if(board[2][0] === 'X' && board[1][1] === 'X' && board[0][2]===''){
+        move(button3, 0, 2);
+    } else if(board[2][0] === 'X' && board[0][2] === 'X' && board[1][1]===''){
+        move(button5, 1, 1);
+    } else if(board[1][1] === 'X' && board[0][2] === 'X' && board[2][0]===''){
+        move(button7, 2, 0);
+    }
+
+    // Mitte angreifen
+    else if(board[1][1] === '' && button5){
+        move(button5, 1, 1);
+    }
+
+    // Ecken angreifen
+    else if(board[0][0] === ''){
+        move(button1, 0, 0);
+    } else if(board[0][2] === ''){
+        move(button3, 0, 2);
+    } else if(board[2][0] === ''){
+        move(button7, 2, 0);
+    } else if(board[2][2] === ''){
+        move(button9, 2, 2);
+    }
+    // Seiten angreifen
+    else if(board[0][1] === ''){
+        move(button2, 0, 1);
+    } else if(board[1][0] === ''){
+        move(button4, 1, 0);
+    } else if(board[1][2] === ''){
+        move(button6, 1, 2);
+    } else if(board[2][1] === ''){
+        move(button8, 2, 1);
+    }
+}
+
+
+function checkWin() {
+    const lines = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[2][0], board[1][1], board[0][2]]
+    ];
+    return lines.some(line => line.every(cell => cell === currentPlayer));
+}
+
+function isDraw() {
+    return board.flat().every(cell => cell !== '');
 }
 
 function checkWin() {
